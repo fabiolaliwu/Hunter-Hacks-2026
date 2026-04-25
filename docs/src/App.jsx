4211639af8adcf8fbe340f5d8c209111c1e73392
I-Asous <islamasous@gmail.com>
2026-04-25 15:12:31 -0400

    commented the routes

--- docs/src/App.jsx
+++ docs/src/App.jsx
@@ -1,17 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./pages/landing/landing";
-import AI from "./pages/ai/ai";
-import Map from "./pages/map/map";
-import Profile from "./pages/profile/profile";
+// uncomment everything once the jsx are returning something
+// import AI from "./pages/ai/ai";
+// import Map from "./pages/map/map";
+// import Profile from "./pages/profile/profile";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="/AI" element={<AI />} />
+        {/* <Route path="/AI" element={<AI />} />
         <Route path="/Map" element={<Map />} />
-        <Route path="/Profile" element={<Profile />} />
+        <Route path="/Profile" element={<Profile />} /> */}
       </Routes>
     </BrowserRouter>
   );
