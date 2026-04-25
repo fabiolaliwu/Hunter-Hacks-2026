a6da9ee4bf497a4f7525befc9c38a5827562a187
Leila Moumou <89880143+Leilamoumou@users.noreply.github.com>
2026-04-25 16:55:43 +0000

    map && more

--- docs/map/map.jsx
+++ docs/map/map.jsx
@@ -1,2 +1,26 @@
-<!doctype html>
-<html lang="en">
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+
+const containerStyle = {
+  width: '100%',
+  height: '400px'
+};
+
+const center = {
+  lat: 40.7128,
+  lng: -74.0060
+};
+
+function MyMap() {
+  return (
+<LoadScript googleMapsApiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={10}
+      >
+        <Marker position={center} />
+      </GoogleMap>
+    </LoadScript>
+  );
+}
+
+export default MyMap;
