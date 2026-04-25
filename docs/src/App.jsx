f71419f8134f1b55308a937d76d23b2b3a8bb311
I-Asous <islamasous@gmail.com>
2026-04-25 15:11:12 -0400

    routes are working

--- docs/src/App.jsx
+++ docs/src/App.jsx
@@ -1,11 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./pages/landing/landing";
+import AI from "./pages/ai/ai";
+import Map from "./pages/map/map";
+import Profile from "./pages/profile/profile";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/AI" element={<AI />} />
+        <Route path="/Map" element={<Map />} />
+        <Route path="/Profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
